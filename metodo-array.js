b5f339c9b4e52477aeb2a13abfe9e764978171e4
Rafaela Huther <huther.rafaela@gmail.com>
2025-06-14 16:59:04 -0300

    Adiciona atividade de método array

---
+++ metodo-array.js
@@ -0,0 +1,33 @@
+//1
+// let numbers = [1, 2, 3, 4, 5]
+// let dobro = numbers.map((numbers) => {
+//     return numbers * 2
+// });
+// console.log(dobro);
+
+//2
+// let number = [1, 2, 3, 4, 5]
+// const pares = number.filter((number) => {
+//     return number % 2 === 0;
+// });
+// console.log(pares);
+
+//3
+// let fruits = ['apple', 'banana', 'orange', 'kiwi']
+// fruits.forEach((fruits) => {
+//     console.log(fruits);
+// });
+
+//4
+// students = [{ name: 'John', age: 20 }, { name: 'Jane', age: 25 }, { name: 'Mark', age: 22 }
+// ]
+// students.forEach(students => {
+// console.log(`O nome do estudante é ${students.name} e ele tem ${students.age} anos.`);
+// });
+
+//5
+// students = [{ name: 'John', age: 20 }, { name: 'Jane', age: 25 }, { name: 'Mark', age: 22 }
+// ]
+// students.map(students => {
+// console.log(`O nome dos estudantes são:${students.name}`);
+// });
