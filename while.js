0a3c4398340280b47a7fb6e5a23065cbd80abe1e
Rafaela Huther <huther.rafaela@gmail.com>
2025-06-14 13:43:33 -0300

    Adiciona atividade de while

---
+++ while.js
@@ -0,0 +1,39 @@
+// Atividade 1
+// let i = 1;
+
+// while (i <= 10) {
+//     i++;
+//     console.log(i);
+// }
+
+// Atividade 2
+// let numero;
+// let soma = 0;
+
+// do {
+//     numero = Number(prompt("Digite um número para a soma e 0 para finalizar"));
+//     soma = soma + numero;
+//     console.log(`A soma é: ${soma}`);
+// } while (numero !== 0);
+
+// Atividade 3
+// let numero = Number(prompt("Digite um número"));
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${numero} x ${i} = ${i * numero}`); // 1 x 20 = 20
+// }
+
+// Atividade 4
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// Atividade 5
+// let senha;
+
+// do {
+//     senha = prompt("Digite a sua senha");
+//     console.log(senha);
+// } while (senha !== "1234");
